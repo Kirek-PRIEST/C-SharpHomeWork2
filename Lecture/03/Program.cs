87a5c1b3069a2046ed17738468e205c8f7f036ce
Макс Просто <kirek@mail.ru>
2023-10-08 19:50:21 +0500

    Лекция закончилась

---
+++ Lecture/03/Program.cs
@@ -0,0 +1,14 @@
+﻿// Поиск индекса массива с указанным значением
+int[] array = {1,2,4,5,6,7,8,5,10};
+int index = 0;
+int find = 5;
+int n = array.Length;
+while (n > index){
+if (find == array[index]) {
+    Console.WriteLine("Число в индексе " + index);
+    break;
+    }
+index++;
+}
+
+
---
+++ Lecture/04/Program.cs
@@ -0,0 +1,54 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+        {
+
+        collection[index] = new Random().Next(1,10);
+        index++;
+        }
+
+
+
+}
+
+void PrintArray(int[] col)
+    {
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+        {
+        Console.WriteLine(col[position]);
+        position++;
+        } 
+    }
+
+int IndexOf(int[] collectiot, int find)
+{
+    int count = collectiot.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (find == collectiot[index]) 
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+
+return position;
+}
+
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+Console.Write("Элемент в позиции ");
+
+int pos = IndexOf(array, 4);
+Console.Write(pos);
+
