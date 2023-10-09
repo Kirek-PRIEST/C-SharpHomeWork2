ead8ad6e3c4a7f99c61ffd21213fc2d68757b083
Макс Просто <kirek@mail.ru>
2023-10-10 01:04:12 +0500

    ДЗ 3

---
+++ HW/03 (случайно)/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+string ReadString(string msg)
+{
+
+    Console.WriteLine(msg);
+    string day = Console.ReadLine();
+    string lowerDay = day.ToLower();
+    return lowerDay;
+}
+string a = ReadString("Введите день недели:");
+int b = 0;
+if (a == "понедельник")
+{
+    b = 1;
+    Console.WriteLine(b);
+}
+if (a == "вторник")
+{
+    b = 2;
+    Console.WriteLine(b);
+}
+if (a == "среда")
+{
+    b = 3;
+    Console.WriteLine(b);
+}
+if (a == "четверг")
+{
+    b = 4;
+    Console.WriteLine(b);
+}
+if (a == "пятница")
+{
+    b = 5;
+    Console.WriteLine(b);
+}
+if (a == "суббота")
+{
+    b = 6;
+    Console.WriteLine(b);
+}
+if (a == "воскресенье")
+{
+    b = 7;
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
---
+++ HW/03/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+int ReadInt(string msg)
+{
+
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int a = int.Parse(numb);
+    return a;
+}
+int a = ReadInt("Введите порядковый номер дня недели:");
+
+if (a > 1 & a < 8)
+{
+    if (a == 6 | a == 7)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
+else { Console.WriteLine("такого дня недели не существует"); }
